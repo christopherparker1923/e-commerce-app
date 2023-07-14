0f664b7c555908d29a0eb279918e59e3f31b3096
christopherparker1923 <christmas1923@gmail.com>
2023-07-14 17:41:41 -0400

    Adding new pages for content

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,50 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+  {
+    title: "catalog",
+    path: "/catalog",
+  },
+  {
+    title: "about",
+    path: "/about",
+  },
+  {
+    title: "contact",
+    path: "/contact",
+  },
+];
+
+const rightLinks = [
+  {
+    title: "login",
+    path: "/login",
+  },
+  {
+    title: "register",
+    path: "/register",
+  },
+];
+
+const navStyles = {
+  color: "inherit",
+  textDecoration: "none",
+  typography: "h6",
+  "&:hover": { color: "grey.500" },
+  "&.active": { color: "text.secondary" },
+};
 
 interface Props {
   darkMode: boolean;
@@ -8,9 +54,41 @@ interface Props {
 export default function Header({ darkMode, setDarkMode }: Props) {
   return (
     <AppBar position="sticky" sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography variant="h6">Ski Shop</Typography>
-        <Switch checked={darkMode} onChange={setDarkMode} />
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6" component={NavLink} to="/" sx={navStyles}>
+            Ski Shop
+          </Typography>
+          <Switch checked={darkMode} onChange={setDarkMode} />
+        </Box>
+
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem sx={navStyles} component={NavLink} to={path} key={path}>
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <Box display="flex" alignItems="center">
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <ShoppingCart />
+            <Badge badgeContent={4} color="secondary" />
+          </IconButton>
+
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem sx={navStyles} component={NavLink} to={path} key={path}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,25 @@
+import { Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/product";
+
+export default function ProductDetails() {
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <Typography variant="h3">Loading...</Typography>;
+
+  if (!product) return <Typography variant="h3">Product not found</Typography>;
+
+  return <Typography variant="h2">{product.name}</Typography>;
+}
