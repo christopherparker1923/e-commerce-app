a171a45a3018064b053597e296ec0df2660a177a
christopherparker1923 <christmas1923@gmail.com>
2023-07-16 21:31:34 -0400

    Adding exception handling

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,18 @@
-import { Typography } from "@mui/material";
+import {
+  Divider,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
 
 export default function ProductDetails() {
   const { id } = useParams();
@@ -21,5 +31,48 @@ export default function ProductDetails() {
 
   if (!product) return <Typography variant="h3">Product not found</Typography>;
 
-  return <Typography variant="h2">{product.name}</Typography>;
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={6}>
+        <img
+          src={product.pictureUrl}
+          alt={product.name}
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid sx={{ mt: 4 }} xs={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">{`$${(
+          product.price / 100
+        ).toFixed(2)}`}</Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>{product.type}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Brand</TableCell>
+                <TableCell>{product.brand}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Stock</TableCell>
+                <TableCell>{product.quantityInStock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
+  );
 }
